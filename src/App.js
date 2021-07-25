3b74f72b5d69c3c8e720332f43aebb7e6e3ae752
barnabasmolnar <barna.molnar90@gmail.com>
2021-07-25 22:00:16 +0200

    Added tailwind.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-12">
+      <h1 className="text-4xl text-indigo-700">Hello there!</h1>
+      <h2 className="text-4xl text-red-700">General Kenobi!</h2>
+      <div className="mt-6 w-[510px] h-[525px] bg-gray-700 text-white flex items-center justify-center">
+        JITtery stuff going on here.
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
